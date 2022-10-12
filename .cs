95798df18d05054fe65985f690aca746bea06dde
cballard2023 <112350914+cballard2023@users.noreply.github.com>
2022-10-12 18:09:53 -0400

    Create .cs
    finished project

---
+++ .cs
@@ -0,0 +1,18 @@
+Console.WriteLine( Math.Min (1, 100) );
+Console.WriteLine(Math. Min(2, 24) );
+
+
+
+List<int> lst = new List<int>(1, 100);
+
+List<int> lst = new List<int>(1, 60);
+
+List<int> lst = new List<int>(26, 60);
+
+
+for (i = 1; i <= 25; i++)
+
+for (i = 1; i <= 60; i++)
+
+        if (x >= 1 && x <= 100)
+
